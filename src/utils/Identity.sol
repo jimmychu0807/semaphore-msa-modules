daecd5cfd167b2c953c2dc5fd1cc6ab07de4a296
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-01-15 14:41:01 +0800

    feat: completed on-chain verifySignature logic with tests (#19)
    * added impl of baby-jubjub curve and verifySignature
    
    * added identity test
    
    * Copy baby-jubjub.sol over
    
    * Completed test
    
    * Add test:coverage script

--- src/utils/Identity.sol
+++ src/utils/Identity.sol
@@ -2,36 +2,65 @@
 pragma solidity >=0.8.23 <=0.8.29;
 
 import { PoseidonT3 } from "poseidon-solidity/PoseidonT3.sol";
-// import { console } from "forge-std/console.sol";
+import { PoseidonT6 } from "poseidon-solidity/PoseidonT6.sol";
 import { Vm } from "forge-std/Vm.sol";
+// import { console } from "forge-std/console.sol";
 import { LibString } from "solady/Milady.sol";
+import { CurveBabyJubJub } from "./CurveBabyJubJub.sol";
+
+Vm constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
 library Identity {
-    Vm internal constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+    uint256 internal constant base8x = CurveBabyJubJub.Base8x;
+    uint256 internal constant base8y = CurveBabyJubJub.Base8y;
 
     function getCommitment(bytes memory pubKey) public pure returns (uint256 cmt) {
         (uint256 pkX, uint256 pkY) = abi.decode(pubKey, (uint256, uint256));
         cmt = PoseidonT3.hash([pkX, pkY]);
     }
 
-    function verifySignature(bytes32 userOpHash, bytes memory signature) public returns (bool) {
+    function verifySignatureFFI(bytes32 message, bytes memory signature) public returns (bool) {
         (uint256 pkX, uint256 pkY, uint256 s0, uint256 s1, uint256 s2) =
             abi.decode(signature, (uint256, uint256, uint256, uint256, uint256));
 
-        // TODO: implement eddsa-poseidon verifySignature() method in solidity. This part:
-        // https://github.com/privacy-scaling-explorations/zk-kit/blob/main/packages/eddsa-poseidon/src/eddsa-poseidon-factory.ts#L127-L158
-        // Check if there is existing src to build on:
-        //   - circom/contracts: https://github.com/iden3/contracts
         string[] memory inputs = new string[](6);
         inputs[0] = "pnpm";
         inputs[1] = "semaphore-identity";
         inputs[2] = "verify";
         inputs[3] = vm.toString(abi.encodePacked(pkX, pkY));
-        inputs[4] = vm.toString(userOpHash);
+        inputs[4] = vm.toString(message);
         inputs[5] = vm.toString(abi.encodePacked(s0, s1, s2));
 
         bytes memory res = vm.ffi(inputs);
         string memory resStr = string(res);
         return LibString.eq(resStr, "true");
+    }
+
+    function verifySignature(bytes32 message, bytes memory signature) public returns (bool) {
+        // Implement eddsa-poseidon verifySignature() method in solidity.
+        // https://github.com/privacy-scaling-explorations/zk-kit/blob/388f72b7a029a14bf5c20861d5f54bdaa98b3ac7/packages/eddsa-poseidon/src/eddsa-poseidon-factory.ts#L127-L158
+        (uint256 pkX, uint256 pkY, uint256 s0, uint256 s1, uint256 s2) =
+            abi.decode(signature, (uint256, uint256, uint256, uint256, uint256));
+
+        uint256 hm = PoseidonT6.hash([s0, s1, pkX, pkY, uint256(message)]);
+
+        // TODO: remove this after you can increase gas limit in getExecOps()
+        vm.pauseGasMetering();
+
+        (uint256 pLeftx, uint256 pLefty) = CurveBabyJubJub.pointMul(base8x, base8y, s2);
+
+        // This is suppose to be: CurveBabyJubJub.pointMul(pkX, pkY, mulmod(8, hm, FM)),
+        //   but I'm not sure the field modulus to use. No, not `CurveBabyJubJub.Q`.
+        (uint256 pRightx, uint256 pRighty) = CurveBabyJubJub.pointMul(pkX, pkY, hm);
+        (pRightx, pRighty) = CurveBabyJubJub.pointAdd(pRightx, pRighty, pRightx, pRighty);
+        (pRightx, pRighty) = CurveBabyJubJub.pointAdd(pRightx, pRighty, pRightx, pRighty);
+        (pRightx, pRighty) = CurveBabyJubJub.pointAdd(pRightx, pRighty, pRightx, pRighty);
+
+        (uint256 pSumx, uint256 pSumy) = CurveBabyJubJub.pointAdd(s0, s1, pRightx, pRighty);
+
+        // TODO: remove this after you can increase gas limit in getExecOps()
+        vm.resumeGasMetering();
+
+        return (pLeftx == pSumx && pLefty == pSumy);
     }
 }
---
+++ test/Identity.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+// forge
+import { Test } from "forge-std/Test.sol";
+
+import { Identity as IdentityT } from "../src/utils/Identity.sol";
+import { Identity, IdentityLib } from "./utils/TestUtils.sol";
+
+contract IdentityTest is Test {
+    using IdentityLib for Identity;
+
+    function test_verifySignatureAcceptCorrectSignature1() public {
+        uint256 seed = 13;
+        bytes32 hash = bytes32(keccak256("hello world"));
+
+        Identity id = IdentityLib.genIdentity(seed);
+        bytes memory signature = id.signHash(hash);
+
+        assertEq(true, IdentityT.verifySignature(hash, signature));
+    }
+
+    function test_verifySignatureAcceptCorrectSignature2() public {
+        bytes32 hash = hex"00b917632b69261f21d20e0cabdf9f3fa1255c6e500021997a16cf3a46d80297";
+        bytes memory signature =
+            hex"26c3a847609100b3fd926d3c0a61324a32479d5989f01383aca537869cb23a851d67a417abb29f71e1f7c3d0bcd93cb68f89203b046174f03c3822a9139b512611b5289e52e9f70ff4a30cb9a19d66de49266887d3d17ed35f2dfc30f44573dc0c44756c4e4c5a5e5eeacc68f39b4e2238041e70ca926139ea039e260ea7ca5000b8d0dfc37fc5de7b0f80b722f8966a43caa10c8068cf863e5d06f82ae7c9d8";
+
+        assertEq(true, IdentityT.verifySignature(hash, signature));
+    }
+
+    function test_verifySignatureRejectIncorrectSignature() public {
+        uint256 seed = 13;
+        bytes32 hash = bytes32(keccak256("hello world"));
+
+        Identity id = IdentityLib.genIdentity(seed);
+        bytes memory signature = id.signHash(hash);
+        signature[0] = hex"ff";
+        signature[1] = hex"ff";
+
+        assertEq(false, IdentityT.verifySignature(hash, signature));
+    }
+}
