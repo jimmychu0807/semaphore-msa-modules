861c81024d23afa9e48ea7952a500f9b78257200
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-02-24 18:06:27 +0800

    added instructions ui

---
+++ packages/web/src/components/layout/footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://jimmychu0807.hk/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+        Blog Post
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://github.com/jimmychu0807/semaphore-msa-modules"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+        Source
+      </a>
+    </footer>
+  );
+}
