b8debc2254a698b0f67b64010a4aa25484baf1b4
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-02-28 14:41:35 +0800

    working on identity

--- packages/web/src/components/Steps.tsx
+++ packages/web/src/components/Steps.tsx
@@ -2,27 +2,40 @@
 
 import { type Address } from "viem";
 import { useAccount } from "wagmi";
+import { useState } from "react";
 
 import { Button } from "./Button";
+
+const TEST_IDENTITY_CMT = 5926199064674296336324214663897687094599930109695195936304576785549579890551n;
 
 export function Steps() {
   const account = useAccount();
   const isConnected = !!account.address;
+  const [identity, setIdentity] = useState();
 
-  if (!isConnected) return <ConnectWithWallet />;
+  if (!isConnected) {
+    return <div className="self-center">Please connect with your wallet</div>;
+  }
 
-  function createIdentity(address: Address) {
-    console.log(address);
+  function createIdentity() {
+    setIdentity(TEST_IDENTITY_CMT);
+  }
+
+  function forgetIdentity() {
+    setIdentity(null);
   }
 
   return (
     <section className="self-center flex flex-col items-center">
-      <h3>Identity</h3>
-      <Button buttonText="Create an Identity" onClick={() => createIdentity(account.address!)} />
+      <h3 className="py-2 font-semibold">Identity</h3>
+      { !!identity
+        ? <>
+          <div>Your Identity</div>
+          <div>{identity}</div>
+          <Button buttonText="Forget this Identity" onClick={ () => forgetIdentity() }/>
+          </>
+        : <Button buttonText="Create an Identity" onClick={() => createIdentity()} />
+      }
     </section>
   );
-}
-
-export function ConnectWithWallet() {
-  return <div>Please connect with your wallet.</div>;
 }
